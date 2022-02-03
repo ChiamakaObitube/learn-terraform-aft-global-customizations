f7a5cb76da53a10dbd3e17c6a82b30096d7e081e
chiamakaobitube <obitubechiamaka@gmail.com>
2022-02-03 20:24:13 +0100

    add new ssm

--- terraform/system-mananger.tf
+++ terraform/system-mananger.tf
@@ -1,11 +1,11 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_ssm_parameter" "secret" {
-  name        = "/${data.aws_caller_identity.current.account_id}/hello"
-  description = "The parameter description"
-  type        = "String"
-  value       = data.aws_caller_identity.current.account_id
-}
+# resource "aws_ssm_parameter" "secret" {
+#   name        = "/${data.aws_caller_identity.current.account_id}/hello"
+#   description = "The parameter description"
+#   type        = "String"
+#   value       = data.aws_caller_identity.current.account_id
+# }
 
 resource "aws_ssm_parameter" "nvio" {
   name        = "/${data.aws_caller_identity.current.account_id}/hello"
