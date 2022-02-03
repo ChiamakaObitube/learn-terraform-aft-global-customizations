b025ab785c012ba8910dc0bdeee0892ecdff96f0
chiamakaobitube <obitubechiamaka@gmail.com>
2022-02-03 20:26:43 +0100

    create s3 bucket

--- terraform/system-mananger.tf
+++ terraform/system-mananger.tf
@@ -1,13 +1,6 @@
 data "aws_caller_identity" "current" {}
 
-# resource "aws_ssm_parameter" "secret" {
-#   name        = "/${data.aws_caller_identity.current.account_id}/hello"
-#   description = "The parameter description"
-#   type        = "String"
-#   value       = data.aws_caller_identity.current.account_id
-# }
-
-resource "aws_ssm_parameter" "nvio" {
+resource "aws_ssm_parameter" "secret" {
   name        = "/${data.aws_caller_identity.current.account_id}/hello"
   description = "The parameter description"
   type        = "String"
@@ -15,4 +8,12 @@ resource "aws_ssm_parameter" "nvio" {
 }
 
 
+resource "aws_s3_bucket" "test" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
 
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
